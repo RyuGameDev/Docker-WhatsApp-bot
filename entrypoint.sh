a038640c27ff9bedc3a9907ec248cfed2069e175
Ryujin <127178263+RyuGameDev@users.noreply.github.com>
2024-01-10 22:12:29 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+clear
+echo "Starting server, wait.."
 cd /home/container
+neofetch --stdout
 
 # Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
 # Print Node.js Version
-node -v
+echo "Using NodeJS:" && node -v
 
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
